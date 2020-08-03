da51b82eed7d734c4d8734bde77b379a39e9e996
Andrea Montoya <tecnicosindependencia@gmail.com>
2020-08-03 09:05:27 -0300

    correcaomodule

--- frontend/src/app/components/product/create/product.service.ts
+++ frontend/src/app/components/product/create/product.service.ts
@@ -1,7 +1,7 @@
 import { Product } from './product.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar} from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,7 +11,7 @@ export class ProductService {
 
   baseUrl =  "http://localhost:3001/products"
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClientModule ) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient ) { }
 
   showMessage(msg: string): void{
     this.snackBar.open(msg, 'X', {
